cd3e3b8cc79bc5cdd66beb119c5521b2cf81a3d7
De Ruvo Daniel <deruvodaniel@gmail.com>
2020-10-22 02:02:15 -0300

    CRUD with php, mysql

---
+++ save_task.php
@@ -0,0 +1,29 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+include("db.php");
+
+if (isset($_POST['save_task'])){
+    
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+
+    
+    if (!$result) {
+        die("Query Failed");
+
+
+    }
+
+    $_SESSION['message'] = 'Task Saved Succesfully';
+    $_SESSION['message_type'] = 'success';
+
+    header("Location: index.php");
+    
+}
+
+?>
